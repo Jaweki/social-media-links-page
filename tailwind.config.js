031b40c10023cc7cc95a8acc8911f7f5b6258327
Jaweki <werukioni@gmail.com>
2024-02-09 00:10:37 +0300

    starter content, tailwindcss, postcss and autoprefixer installed; with design guide resources

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      screen: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+      colors: {
+        primary: {
+          green: "hsl(75, 94%, 57%)",
+        },
+        neutral: {
+          white: "hsl(0, 0%, 100%)",
+          grey: "hsl(0, 0%, 20%)",
+          'dark-grey': "hsl(0, 0%, 12%)",
+          'off-black': "hsl(0, 0%, 8%)",
+        }
+      },
+      fontSize:{
+        paragraph: "14px",
+      },
+      fontFamily: {
+        inter: ["Inter", "Sans"],
+      },
+      fontWeight: {
+        regular: "400",
+        medium: "600",
+        bold: "700",
+      }
+    },
   },
   plugins: [],
 }
