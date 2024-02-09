aeb97d6d61b74e111df033f3072653383e5d2c12
Jaweki <werukioni@gmail.com>
2024-02-09 14:29:14 +0300

    removed h-[65%] from the div with bg-neutral-dark-grey; applied an empty alt to the img element; repalced double quotes with html encoded once; added a key attribute to the button component used in array map

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ function App() {
 
   return (
     <main className="w-screen h-screen bg-neutral-off-black flex justify-center items-center">
-      <div className="h-[65%] w-[320px] bg-neutral-dark-grey rounded-lg flex flex-col items-center justify-center space-y-7 p-6" >
+      <div className=" w-[320px] bg-neutral-dark-grey rounded-lg flex flex-col items-center justify-center space-y-7 p-6" >
 
-        <img src={"./images/avatar-jessica.jpeg"} className="rounded-full h-20 w-20"/>
+        <img src={"./images/avatar-jessica.jpeg"} alt="" className="rounded-full h-20 w-20"/>
         <p>
           <h1 className="text-xl font-bold text-white">Jessica Randall</h1>
           <span className="text-xs font-bold text-primary-green">
@@ -18,7 +18,7 @@ function App() {
         <p className="text-xs text-white"> "Front-end developer and avid reader."</p>
         <div className="flex flex-col w-full space-y-3">
 
-          { buttonText.map((text) => <Button text={text} />)
+          { buttonText.map((text) => <Button key={text} text={text} />)
           }
         </div>
       </div>
